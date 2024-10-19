d29a389db785955f8d217e713d2488a3b19117f9
Marta-ms <Marta-ms>
2024-10-19 17:28:05 +0200

    añadimos type:module a form

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
 'use strict';
 
-import"./card";
-import"./form-design";
-import"./form-rellena";
-import"./form-comparte";
+
+import "./card";
+import "./form-design";
+import "./form-rellena";
+import "./form-comparte";
+import "./preview";
+
+
+// cuando la usuaria escriba en el input, 
+// recoger la informacion y mostarla en card
+
+
+
+
--- src/js/preview.js
+++ src/js/preview.js
@@ -1,8 +1,9 @@
-'use strict'
+
 
 const inputName = document.querySelector ("js-name");
 const inputText = document.querySelector ("js-text");
 const inputImage = document.querySelector ("js-image");
 const inputMail = document.querySelector ("js-mail");
 const inputNumber = document.querySelector ("js-address");
-const inputAdress = document.querySelector ("js-number");
+const inputAddress = document.querySelector ("js-number");
+
