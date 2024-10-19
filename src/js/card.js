051917f0fce5cd459dc573dde2ce9946cf526c8d
Nuria Campo <nuria.campo7@gmail.com>
2024-10-19 20:55:38 +0200

    previsualización funciona

---
+++ src/js/card.js
@@ -0,0 +1,43 @@
+// Constantes de formRellena
+const formRellena = document.querySelector('.js-fieldset-rellena');
+const inputNameAddressee = document.querySelector('.js-input-nameAddressee');
+const inputMessage = document.querySelector('.js-input-message');
+const inputImg = document.querySelector('.js-input-img');
+const inputPhone = document.querySelector('.js-input-phone');
+const inputNameSender = document.querySelector('.js-input-nameSender');
+const inputInsta = document.querySelector('.js-input-insta');
+
+// Constantes de card
+const cardNameAddressee = document.querySelector('.js-card-nameAddressee');
+const cardMessage = document.querySelector('.js-card-message');
+const cardImg = document.querySelector('.js-card-img');
+const cardPhone = document.querySelector('.js-card-phone');
+const cardNameSender = document.querySelector('.js-card-nameSender');
+const cardInsta = document.querySelector('.js-card-insta');
+
+/* Cuando la usuaria escriba en el input, quiero que se previsualice en el card.
+1. Recoger todos los inputs de rellena
+2. Recoger todos los p de card
+3. Cuando escriba en el input
+  - recoger el valor del input
+  - poner en el html de card lo que vale el input
+*/
+
+formRellena.addEventListener ('input', event => {
+  console.log('event.target.value es:', event.target.value);
+  console.log('event.target.id es:', event.target.id);
+  
+  if (event.target.id === 'nameAddressee') {
+    cardNameAddressee.innerHTML = event.target.value;
+  } else if (event.target.id === 'message') {
+    cardMessage.innerHTML = event.target.value;
+  } else if (event.target.id === 'img') {
+    cardImg.innerHTML = event.target.value;
+  } else if (event.target.id === 'phone') {
+    cardPhone.innerHTML = event.target.value;
+  } else if (event.target.id === 'nameSender') {
+    cardNameSender.innerHTML = event.target.value;
+  } else if (event.target.id === 'insta') {
+    cardInsta.innerHTML = event.target.value;
+  }
+})
--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,11 @@
 'use strict';
 
-
-import "./card";
 import "./form-design";
 import "./form-rellena";
 import "./form-comparte";
-import "./preview";
+import "./card";
 
 
-// cuando la usuaria escriba en el input, 
-// recoger la informacion y mostarla en card
 
 
 
