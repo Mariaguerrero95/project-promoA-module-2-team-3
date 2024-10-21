8e1d675a5bef9f328f6a224a0833f546eb0bd106
Tania <nadsanp@gmail.com>
2024-10-21 18:26:46 +0200

    javascript start

---
+++ src/js/form-design.js
@@ -0,0 +1,8 @@
+/* Cuando la usuaria haga click en la casilla, cambia el fondo de la tarjeta
+    Pasos:
+    - Selecciono los elementos html: input de la casilla y la tarjeta
+    - Si la usuaria hace click, se cambia el fondo de pantalla deseado
+*/
+const background1 = document.querySelector(".js-nubes");
+const card1 = document.querySelector(".js-card");
+
