0a7f93034fd3075ff1f66983426b8f40dcb8f537
Tania <nadsanp@gmail.com>
2024-10-22 13:22:59 +0200

    background change

--- src/js/form-design.js
+++ src/js/form-design.js
@@ -3,11 +3,31 @@
     - Selecciono los elementos html: input de la casilla y la tarjeta
     - Si la usuaria hace click, se cambia el fondo de pantalla deseado
 */
-const fieldset = document.querySelector("js-fieldset-diseña");
+const fieldset = document.querySelector(".js-fieldset-diseña");
 const background1 = document.querySelector(".js-azul");
-const background2 = document.querySelector("js-amarillo");
-const background3 = document.querySelector ("js-verde");
+const background2 = document.querySelector(".js-amarillo");
+const background3 = document.querySelector (".js-verde");
 const card1 = document.querySelector(".js-card");
+
+// background1.addEventListener("click", () => {
+//     card1.classList.toggle ("card-background-blue");
+
+// })
+fieldset.addEventListener("click", (event) =>{
+    console.log("event");
+    if (event.target.id === "option1"){
+        card1.classList.toggle("card-background-blue");
+        
+    } else if (event.target.id === "option2"){
+        card1.classList.toggle ("card-background-yellow");
+    } else if ( event.target.id === "option3"){
+        card1.classList.toggle ("card-background-green");
+
+    }    
+})
+
+
+
 
 
 
