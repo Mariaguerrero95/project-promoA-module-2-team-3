7d4791a95b1b623b1c17b296023790a70b2f906d
Marta-ms <Marta-ms>
2024-10-24 16:16:54 +0200

    hago un pequeño cambio en rellena

--- src/js/form-rellena.js
+++ src/js/form-rellena.js
@@ -6,3 +6,18 @@ const inputImg = document.querySelector(".js-input-img");
 const inputPhone = document.querySelector(".js-input-phone");
 const inputNameSender = document.querySelector(".js-input-nameSender");
 const inputInsta = document.querySelector(".js-input-insta");
+
+
+
+const legendFillout = document.querySelector(".js-legend-share");
+const formFillout = document.querySelector(".js-form-share");
+const arrowFillout = document.querySelector(".js-icon-arrow-share");
+
+legendShare.addEventListener("click", dropDown);
+
+function dropDown() {
+  formShare.classList.toggle("collapse");
+
+  arrowShare.classList.toggle("rotate");
+  console.log("clase rotate");
+}
