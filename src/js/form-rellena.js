a919299d92d3c9ce5ab2f9d49bc15d303c3cd471
Nuria Campo <nuria.campo7@gmail.com>
2024-10-24 19:29:43 +0200

    desplegable de rellena funciona

--- src/js/form-rellena.js
+++ src/js/form-rellena.js
@@ -6,3 +6,16 @@ const inputImg = document.querySelector(".js-input-img");
 const inputPhone = document.querySelector(".js-input-phone");
 const inputNameSender = document.querySelector(".js-input-nameSender");
 const inputInsta = document.querySelector(".js-input-insta");
+
+const legendFillOut = document.querySelector(".js-legend-fillout");
+const arrowFillOut = document.querySelector(".js-arrow-fillout");
+const formFillOut = document.querySelector(".js-form-fillout");
+
+
+legendFillOut.addEventListener("click", dropDown1);
+
+function dropDown1() {
+  console.log('click')
+  formFillOut.classList.toggle("collapse");
+  arrowFillOut.classList.toggle("rotate");
+}
