2bb0cdac1da0bf8620a4796f4fba1deb0d1c6197
Marta-ms <Marta-ms>
2024-10-24 14:09:04 +0200

    details

--- src/js/form-rellena.js
+++ src/js/form-rellena.js
@@ -6,3 +6,17 @@ const inputImg = document.querySelector(".js-input-img");
 const inputPhone = document.querySelector(".js-input-phone");
 const inputNameSender = document.querySelector(".js-input-nameSender");
 const inputInsta = document.querySelector(".js-input-insta");
+
+const legendFillOut = document.querySelector(".js-legend-fillout");
+const arrowFillOut = document.querySelector(".js-arrow-fillout");
+const formFillOut = document.querySelector(".js-form-fillout");
+
+legendShare.addEventListener("click", dropDown);
+
+function dropDown() {
+  formFillOut.classList.toggle("collapse");
+
+  arrowFillOut.classList.toggle("rotate");
+  console.log("clase rotate");
+}
+
