49f515b63c7fe89d52b36d0dcb761e42c05e4813
Mariaguerrero95 <mariaguerrero1995@outlook.es>
2024-10-27 20:05:41 +0100

    arrows fixed

--- src/js/form-rellena.js
+++ src/js/form-rellena.js
@@ -19,3 +19,37 @@ function dropDown1() {
   formFillOut.classList.toggle("collapse");
   arrowFillOut.classList.toggle("rotate");
 }
+
+// HACER QUE EL DESPLEGABLE RELLENA ESTÉ ABIERTO AL RECARGAR LA PÁGINA
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const fieldsetRellena = document.querySelector('.js-fieldset-rellena');
+  const fieldsetDiseña = document.querySelector('.js-fieldset-diseña');
+  const fieldsetComparte = document.querySelector('.js-legend-share');
+
+  // Abre solo "RELLENA" al cargar la página
+  fieldsetRellena.classList.add('open');
+
+  // Los otros dos estén cerrados
+  fieldsetDiseña.classList.remove('open');
+  fieldsetComparte.classList.remove('open');
+
+  // Alterna la clase 'open' cuando se hace clic en cada legend
+  
+    document.querySelectorAll('legend').forEach(legend => {
+      legend.addEventListener('click', () => {
+        const parentFieldset = legend.parentElement;
+
+        // Cierra los otros fieldsets al abrir uno
+        if (!parentFieldset.classList.contains('open')) {
+          document.querySelectorAll('fieldset').forEach(fieldset => {
+            fieldset.classList.remove('open');
+          });
+        }
+        
+        // Alterna la clase 'open' en el fieldset seleccionado
+        parentFieldset.classList.toggle('open');
+      });
+    });
+  });
