497ca4637d98b9b54aeb9b1ce6b0bc638e010d1a
Nuria Campo <nuria.campo7@gmail.com>
2024-10-28 22:20:22 +0100

    funcionan los tres desplegables de los formularios

--- src/js/formAll.js
+++ src/js/formAll.js
@@ -1,42 +1,21 @@
-// // Colapsabe de diseña
-
-// legendDesign.addEventListener("click", collapse) 
-
-// function collapse () {
-//     formDesign.classList.toggle ("collapse");
-//     arrowDesign.classList.toggle ("rotate");
-// }
-
-// // Colapsabe de rellena
-// legendFillOut.addEventListener("click", dropDown1);
-// function dropDown1() {
-//   console.log('click')
-//   formFillOut.classList.toggle("collapse");
-//   arrowFillOut.classList.toggle("rotate");
-// }
-
-
-
-
 const formContainer = document.querySelector(".js-div-container__form");
 
-// Colapsabe de comparte
 formContainer.addEventListener("click", dropDown);
 
 function dropDown(event) {
-  if(event.target.classList.contains('js-share-drop-down')) {
-    console.log('he hecho click en comparte')
-    formShare.classList.toggle("collapse");
-    arrowShare.classList.toggle("rotate");
 
-    formDesign.classList.add("collapse");
-    arrowDesign.classList.remove("rotate");
+  if (event.target.classList.contains('js-design-drop-down')){
+    formDesign.classList.toggle("collapse");
+    arrowDesign.classList.toggle("rotate");
 
     formFillOut.classList.add("collapse");
     arrowFillOut.classList.remove("rotate");
 
-    
-  } else if (event.target.classList.contains('js-fillout-drop-down')){
+    formShare.classList.add("collapse");
+    arrowShare.classList.remove("rotate");
+  }
+
+  if (event.target.classList.contains('js-fillout-drop-down')){
     formFillOut.classList.toggle("collapse");
     arrowFillOut.classList.toggle("rotate");
 
@@ -47,4 +26,14 @@ function dropDown(event) {
     arrowShare.classList.remove("rotate");
   }
 
+  if(event.target.classList.contains('js-share-drop-down')) {
+    formShare.classList.toggle("collapse");
+    arrowShare.classList.toggle("rotate");
+
+    formDesign.classList.add("collapse");
+    arrowDesign.classList.remove("rotate");
+
+    formFillOut.classList.add("collapse");
+    arrowFillOut.classList.remove("rotate");
+  }
 }
